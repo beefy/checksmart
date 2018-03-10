338f52e98667eca0fd405b5e5a82a55939dc238e
beefy <nate.schultz@outlook.com>
2018-03-09 19:01:14 -0500

    added new endpoints to make move and resign

--- backend/src/main/java/checksmart/GameController.java
+++ backend/src/main/java/checksmart/GameController.java
@@ -6,14 +6,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GameController {
+public class GameController{
 
     private static final String template = "%s's Checker Game";
-    private final AtomicLong counter = new AtomicLong();
+    private final AtomicLong game_id = new AtomicLong();
+    private final AtomicLong move_id = new AtomicLong();
 
     @RequestMapping("/new")
-    public Game greeting(@RequestParam(value="name", defaultValue="Anon") String name) {
-        return new Game(counter.incrementAndGet(),
+    public Game startgame(@RequestParam(value="name", defaultValue="Anon") String name) {
+        return new Game(game_id.incrementAndGet(),
                             String.format(template, name));
+    }
+
+    @RequestMapping("/move")
+    public Move makemove(@RequestParam(value="move") String move, @RequestParam(value="player") String player) {
+        Move ret = new Move(move_id.incrementAndGet(), move, player);
+        if(ret.isvalid()) {
+            return ret;
+        } else {
+            // throw exception
+            // TODO: implement exception handling
+            //throw new Exception();
+            return ret;
+        }
+    }
+
+    @RequestMapping("/resign")
+    public String resigngame(@RequestParam(value="gameid") AtomicLong gameid, @RequestParam(value="player") String player) {
+        // TODO: search for game by id, error if not found
+        return player;
     }
 }
